d67ca7173c7b206f343687f5283c7eca4c29ba1d
roxgiba <rox.giba@gmail.com>
2023-10-07 10:47:03 +0200

    resume file updated

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
                 {" "}
                 <a
                   className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="/Tech CV 2023.pdf"
+                  href="/resume_tech.pdf"
                   target="_blank"
                 >
                   Resume
